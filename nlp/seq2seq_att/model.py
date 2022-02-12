d0739d4b3151bd49f636378c05ef86fc58aee72a
Naman Kumar <namank2002@gmail.com>
2022-02-12 20:37:19 +0530

    added attention and decoder layers

--- nlp/seq2seq_att/model.py
+++ nlp/seq2seq_att/model.py
@@ -1,10 +1,12 @@
+from json import encoder
 from unicodedata import bidirectional
 import torch
 import torch.nn as nn
+import torch.functional as F
 
-class EncoderRNN(nn.Module):
+class Encoder(nn.Module):
     def __init__(self, input_size, embedding_size, hidden_size, dropout):
-        super(EncoderRNN, self).__init__()
+        super(Encoder, self).__init__()
         '''
             input_size: Length of sentence with start and end tokens
             embedding_size: Size of embedding
@@ -41,3 +43,95 @@ class EncoderRNN(nn.Module):
 
         return outputs, hidden
 
+class Attention(nn.Module):
+    def __init__(self, hidden_size):
+        super(Attention, self).__init__()
+
+        self.attention = nn.Linear(hidden_size*3, hidden_size)
+        self.v = nn.Linear(hidden_size, 1, bias = False)
+
+    def forward(self, hidden: torch.Tensor, encoder_outputs: torch.Tensor):
+
+        # hidden = [batch_size, hidden_size]
+        # encoder_outputs = [batch_size, input_size, 2*hidden_size]
+
+        input_size = encoder_outputs.shape[1]
+
+        # concatenate previous hidden state to every encoder output
+        hidden = hidden.unsqueeze(1).repeat(1, input_size, 1)
+
+        # hidden shape after unsqueeze -> [batch_size, 1, hidden_size]
+        # hidden (finally) = [batch_size, input_size, hidden_size]
+
+        energy = torch.relu(self.attention(torch.cat((hidden, encoder_outputs), dim = 2)))
+
+        # energy = [batch_size, input_size, hidden_size]
+
+        attention: torch.Tensor = self.v(energy)
+        
+        # attention = [batch_size, input_size, 1]
+
+        attention = attention.squeeze(2)
+
+        # attention = [batch_size, input_size]
+
+        return F.softmax(attention, dim = 1)
+
+class Decoder(nn.Module):
+    def __init__(self, output_size, embedding_size, hidden_size, dropout, attention):
+        super(Decoder, self).__init__()
+
+        self.output_size = output_size
+        self.attention = attention
+        self.embedding = nn.Embedding(output_size, embedding_size)
+        self.rnn = nn.GRU((hidden_size*2) + embedding_size, hidden_size, batch_first = True)
+        self.fc = nn.Linear((hidden_size*3) + embedding_size, output_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input, hidden, encoder_outputs):
+        # input = [batch_size]
+        # hidden = [batch_size, hidden_size]
+        # encoder_outputs = [batch_size, input_size, 2*hidden_size]
+
+        # rough flow: embedding + (attention*encoder_outputs) + prev_hidden_state -> rnn -> hidden_state + (attention*encoder_outputs) + embedding -> fc -> output
+
+        input = input.unsqueeze(1)
+
+        # input = [batch_size, 1]
+
+        embedded = self.dropout(self.embedding(input))
+
+        # embedded = [batch_size, 1, embedding_size]
+
+        a = self.attention(hidden, encoder_outputs)
+
+        # a = [batch_size, input_size]
+
+        a = a.unsqueeze(1)
+
+        # a = [batch_size, 1, input_size]
+
+        weighted = torch.bmm(a, encoder_outputs)
+
+        # weighted = [batch_size, 1, 2*hidden_size]
+
+        rnn_input = torch.cat((embedded, weighted), dim = 2)
+
+        # rnn_input = [batch_size, 1, (hidden_size*2) + embedding_size]
+
+        output, hidden = self.rnn(rnn_input, hidden.unsqueeze(0))
+
+        # output = [batch_size, 1, hidden_size]
+        # hidden = [1, batch_size, hidden_size]
+        assert (output == hidden.permute(1, 0, 2)).all()
+
+        embedded = embedded.squeeze(1)
+        output = output.squeeze(1)
+        weighted = weighted.squeeze(1)
+
+        prediction = self.fc(torch.cat((output, weighted, embedded), dim = 1))
+
+        # prediction = [batch_size, output_size]
+
+        return prediction, hidden.squeeze(0)
+        
